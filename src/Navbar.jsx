06c05bae139460c4a8a7d8d8d2473505ec8fb061
Mehdi 'Arcanite' OUESLATI <mehdi6.oueslati@gmail.com>
2022-04-12 09:40:21 +0200

    partially solved the navbar display problem, still working on mobile responsiveness

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,6 +4,7 @@ import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
@@ -48,47 +49,45 @@ function Navbar(props){
               {props.phrases.navSecret(props)}
             </Button>
           </ButtonGroup>
-            <ButtonGroup 
-            /*TODO: get rid of CSS and find a better, mobile-first way of realizing the same thing. Maybe grids?*/
-              style={{"display": "block", "right": "15vw", "position": "absolute"}}
-            >
+          <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: {md: 'flex' }, m: 1 }}>
               <ThemePicker 
                 theme={props.theme}
                 themes={props.themes}
                 themeUID={props.themeUID}
                 changeTheme={props.changeTheme}
               />
-            </ButtonGroup>
-            <ButtonGroup 
-            variant="contained" 
-            size="large"
-            /*TODO: get rid of CSS and find a better, mobile-first way of realizing the same thing. Maybe grids?*/
-            style={{"display": "block", "right": "1vw", "position": "absolute"}}
-          >
-          {(!props.user) &&
-            <div>
-              <Button
-                startIcon={<AppRegistration />}
-                color="primary"
-                onClick={() => props.changePage("signup")}
-              > 
-                {props.phrases.navSignUp(props)}
-              </Button>
-              <Button
-                startIcon={<Login />}
-                color="secondary"
-                onClick={() => props.changePage("login")}
-              > 
-                {props.phrases.navLogIn(props)}
-              </Button>
-            </div>
-          }
-          {(props.user) &&
-            <div>
-              <Avatar alt={props.user.name} src="old_lena.png" />
-            </div>
-          }
-          </ButtonGroup>
+            </Box>
+            <Box sx={{ display: {md: 'flex' }, m: 0.5 }}>
+              <ButtonGroup 
+                variant="contained" 
+                size="large"
+              >
+              {(!props.user) &&
+                <React.Fragment>
+                  <Button
+                    startIcon={<AppRegistration />}
+                    color="primary"
+                    onClick={() => props.changePage("signup")}
+                  > 
+                    {props.phrases.navSignUp(props)}
+                  </Button>
+                  <Button
+                    startIcon={<Login />}
+                    color="secondary"
+                    onClick={() => props.changePage("login")}
+                  > 
+                    {props.phrases.navLogIn(props)}
+                  </Button>
+                </React.Fragment>
+              }
+              {(props.user) &&
+                <Button>
+                  <Avatar alt={props.user.name} src="old_lena.png" />
+                </Button>
+              }
+              </ButtonGroup>
+            </Box>
         </Toolbar>
       </AppBar>
   );
